540e50ff7f912ca73777b865fb433bf726c7cc81
Derek Wyatt <derek@derekwyatt.org>
2010-10-25 10:17:17 -0400

    Added the 'loan pattern' stuff in the Using.scala code... mocked up a sill DB fake and have the result set do _nothing_ like List work just to make it very clear that bmap() is doing something real

---
+++ beginning-scala/Using.scala
@@ -0,0 +1,68 @@
+
+def using[A <: { def close(): Unit }, B](param: A)(f: A => B): B = {
+    try {
+        f(param)
+    } finally {
+        param.close()
+    }
+}
+
+import scala.collection.mutable.ListBuffer
+
+def bmap[T](test: => Boolean)(block: => T): List[T] = {
+    val ret = new ListBuffer[T]
+    while (test) ret += block
+    ret.toList
+}
+
+// -----------------------------------------------------------------
+// Fake DB support code
+
+class FakeDBResultSet {
+    var count = 0
+    var result = ("", 0, false)
+    def next: Boolean = {
+        count match {
+            case 0 => result = ("John Hanemaayer", 50, true)
+            case 1 => result = ("David Raeside", 40, false)
+            case 2 => result = ("Timothy Lofft", 40, true)
+            case 3 => result = ("Curt Beattie", 30, true)
+            case 4 => result = ("Tom Duffney", 50, false)
+            case _ => return false
+        }
+        println("ResultSet set to result " + count)
+        count += 1
+        true
+    }
+
+    def close(): Unit = println("Closing DB ResultSet")
+}
+
+class FakeDBStatement {
+    def close(): Unit = println("Closing DB Statement")
+    def executeQuery = new FakeDBResultSet
+}
+
+class FakeDBConnection {
+    def close(): Unit = println("Closing DB Connection")
+    def createStatement = new FakeDBStatement
+}
+
+// -----------------------------------------------------------------
+
+case class Person(name: String, age: Int, valid: Boolean)
+
+object Person {
+    def findPeople(conn: FakeDBConnection): List[Person] = {
+        using(conn.createStatement) { statement =>
+            using(statement.executeQuery) { resultSet =>
+                bmap(resultSet.next) { resultSet.result match {
+                        case (n: String, a: Int, v: Boolean) => new Person(n, a, v)
+                    }
+                }
+            }
+        } 
+    }
+}
+
+Person.findPeople(new FakeDBConnection).foreach(println(_))
